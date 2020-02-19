3ddda97c716438ceb788f89d905542d4b0f06662
quantumjot <code@arlowe.co.uk>
2020-02-19 20:10:19 +0000

    Added convolutional net, helper functions and notebook

---
+++ helpers/_core.py
@@ -0,0 +1,150 @@
+__author__ = "Alan R. Lowe"
+__email__ = "a.lowe@ucl.ac.uk"
+
+import os
+import random
+
+import numpy as np
+from skimage import io
+from skimage.filters import median as median_filter
+from skimage.transform import resize
+
+import matplotlib.pyplot as plt
+
+from .model import load_model
+
+CHANNEL_ORDER = ['Brightfield', 'GFP', 'RFP']
+STATES = ['interphase', 'prometaphase', 'metaphase', 'anaphase', 'apoptosis', 'unknown']
+
+
+class _DatasetContainer:
+    def __init__(self):
+        self.__data = np.load('./data/test_data.npz')['images']
+        self.__idx = [i for i in range(self.__data.shape[0])]
+
+    def __len__(self):
+        return self.__data.shape[0]
+
+    def get_random(self, num_images=1):
+        assert(num_images>0 and num_images<len(self))
+        random.shuffle(self.__idx)
+        images = [normalize_image(self.__data[i,...]) for i in self.__idx[:num_images]]
+        return images, self.__idx[:num_images]
+
+
+
+# instantiate the dataset container
+dataset = _DatasetContainer()
+
+def get_example_images(num_images=1):
+    return dataset.get_random(num_images=num_images)
+
+
+
+def normalize_image(im):
+    assert(isinstance(im, np.ndarray))
+    assert(im.ndim == 2)
+    im = im.astype(np.float32)
+    mu, std = np.mean(im), np.std(im)
+    normed = (im - mu) / np.max([std, 1./np.prod(im.shape)])
+    return normed
+
+
+
+def remove_outliers(im):
+    assert(isinstance(im, np.ndarray))
+    if im.ndim == 3:
+        for channel in range(im.shape[-1]):
+            med_filter = median_filter(im[...,channel])
+            mask = im[...,channel] > med_filter + 2.*np.std(med_filter)
+            im[...,channel][mask] = med_filter[mask]
+    return im
+
+
+
+def encode_images(path, num_images=1000):
+    """ encode images as a numpy file, with accompanying json file """
+
+    files = []
+
+    for state in STATES:
+        fp = os.path.join(path, state)
+        files += [(state, fp, f) for f in os.listdir(fp) if f.endswith('.tif')]
+
+    # shuffle the images
+    random.shuffle(files)
+
+    images, labels = [], []
+
+    for state, fp, file in files:
+
+        # the the filename and label
+        fn, ext = os.path.splitext(file)
+        label = STATES.index(state)
+
+        # get the cell type, and extract only that fluorescence channel
+        cell_channel = CHANNEL_ORDER.index(fn[-3:])
+
+        im = io.imread(os.path.join(fp, file))
+        im = remove_outliers(im)
+
+        # rescale the image
+        im_resized = resize(im, (32,32), preserve_range=True)
+        images.append(im_resized[...,cell_channel].astype('uint8'))
+        labels.append(label)
+
+    # make this into a large numpy array for saving
+    np_images_annotation = np.stack(images[:num_images], axis=0)
+    np_images_training = np.stack(images[num_images:], axis=0)
+    np_images_training_labels = np.stack(labels[num_images:], axis=0)
+
+    print(np_images_annotation.shape,
+          np_images_training.shape,
+          np_images_training_labels.shape)
+
+    # write out the numpy array
+    np.savez('./data/cell_data.npz', images=np_images_annotation)
+    np.savez('./data/training_data.npz',
+             images=np_images_training,
+             labels=np_images_training_labels)
+
+    # write out the mapping to the original files
+    with open('./data/cell_data.txt', 'w') as file:
+        for label, fp, fn in files[:num_images]:
+            file.write(f'{fp} {fn} \n')
+
+    return images
+
+
+
+def plot_images(images, image_idx, cols=5):
+    """ plot the images and their ID """
+    num_images = len(images)
+    rows = np.ceil(num_images/cols)
+
+    plt.figure(figsize=(16,16))
+    for i in range(num_images):
+        plt.subplot(rows, cols, i+1)
+        plt.imshow(images[i], cmap=plt.cm.gray)
+        plt.title(f'Image #{image_idx[i]}')
+        plt.axis('off')
+    plt.show()
+
+
+
+def validate_annotation(annotation):
+    """ validate the student annotation """
+    # check that this is a dictionary
+    assert(isinstance(annotation, dict))
+
+    # check that it has the correct keys
+    assert(all([k in STATES for k in annotation.keys()]))
+
+    # check to make sure the numbers make sense
+    pass
+
+
+
+
+def load_CNN_model():
+    return load_model()
---
+++ helpers/model.py
@@ -0,0 +1,80 @@
+__author__ = "Alan R. Lowe"
+__email__ = "a.lowe@ucl.ac.uk"
+
+import random
+import numpy as np
+import tensorflow.keras as K
+import tensorflow.keras.layers as KL
+
+# from _core import normalize_image
+
+def simple_CNN(convolutional_features=32,
+               dense_features=128):
+
+    image = KL.Input(shape=[32, 32, 1], name="input")
+
+    conv1 = conv = KL.Conv2D(convolutional_features, 3, activation='relu', padding='valid')(image)
+    pool1 = KL.MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = conv = KL.Conv2D(convolutional_features, 3, activation='relu', padding='valid')(pool1)
+    pool2 = KL.MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    flat1 = KL.Flatten()(pool2)
+
+    logits = KL.Dense(5, activation='linear')(flat1)
+    softmax = KL.Activation('softmax', name='output')(logits)
+
+    return K.Model(inputs=image, outputs=softmax, name="ConvolutionalNeuralNetwork")
+
+
+
+def data_generator(data, batch_size=32):
+
+    num_images = data['images'].shape[0]
+
+    while True:
+
+        to_use = [n for n in range(num_images)]
+        random.shuffle(to_use)
+
+        batch_images, batch_labels = [], []
+
+        while to_use and len(batch_images)<batch_size:
+
+#             print(len(to_use), len(batch_images))
+
+            n = to_use.pop(0)
+
+            image = normalize_image(data['images'][n,...])
+
+            batch_images.append(image[...,np.newaxis])
+            batch_labels.append(data['labels'][n])
+
+        yield ({'input': np.stack(batch_images, axis=0)},
+               {'output': K.utils.to_categorical(np.stack(batch_labels, axis=0), num_classes=5)})
+
+
+
+def train_model(num_epochs=10):
+
+    data = np.load('../data/training_data.npz')
+
+    batch_size = 32
+    num_training_images = data['images'].shape[0]
+
+    model = simple_CNN()
+    model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
+    model.summary()
+    model.fit(data_generator(data, batch_size=batch_size),
+              epochs=num_epochs, steps_per_epoch=num_training_images / batch_size)
+
+    model.save('../data/model.h5')
+
+
+
+def load_model():
+    return K.models.load_model('./data/model.h5')
+
+
+if __name__ == '__main__':
+    train_model(num_epochs=50)
